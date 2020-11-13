69d2170ea596a9a096b78363c29a8fb6ac6c4c62
huhubun <nongzc@outlook.com>
2020-11-13 23:02:34 +0800

    完成基本内容

---
+++ layouts/default.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="clipped"
+      fixed
+      temporary
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="`drawer_nav_item_${i}`"
+          :to="item.to"
+          :href="item.href"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" flat app>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="hidden-sm-and-up"
+      />
+      <v-toolbar-title v-text="title" />
+
+      <v-spacer />
+
+      <v-toolbar-items class="hidden-xs-only">
+        <template v-for="(item, i) in items">
+          <v-btn
+            text
+            v-bind:key="`toolbar_item_${i}_btn`"
+            :to="items[i].to"
+            :href="items[i].href"
+          >
+            {{ items[i].title }}
+          </v-btn>
+        </template>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <nuxt />
+    </v-main>
+
+    <v-footer color="primary" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="footer in footers"
+          :key="footer.href"
+          :href="footer.href"
+          color="white"
+          text
+          class="my-2"
+        >
+          {{ footer.text }}
+        </v-btn>
+        <v-col class="primary pb-4 text-center white--text" cols="12">
+          &copy; {{ new Date().getFullYear() }}
+          <a
+            href="https://huhubun.com"
+            class="text-decoration-none white--text"
+          >
+            huhubun
+          </a>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-home-outline',
+          title: '首页',
+          to: '/'
+        },
+        {
+          icon: 'mdi-file-document-outline',
+          title: '文档',
+          href: 'https://github.com/huhubun/DDNSSharp'
+        },
+        {
+          icon: 'mdi-history',
+          title: '更新记录',
+          href: 'https://github.com/huhubun/DDNSSharp/releases'
+        },
+        {
+          icon: 'mdi-keyboard-return',
+          title: 'BUN.DEV',
+          href: 'https://bun.dev/'
+        }
+      ],
+      title: 'DDNSSharp',
+      footers: [
+        {
+          text: '源代码',
+          href: 'https://github.com/huhubun/DDNSSharp'
+        },
+        {
+          text: 'bun.dev 旗下作品',
+          href: 'https://bun.dev'
+        }
+      ]
+    }
+  }
+}
+</script>
