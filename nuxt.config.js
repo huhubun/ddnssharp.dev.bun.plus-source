b96c5d1731eeb2912d8882cf0edca912003eed56
huhubun <nongzc@outlook.com>
2023-07-18 23:37:11 +0800

    Remove cnzz

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,6 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '@/plugins/cnzz', ssr: false },
     '@/plugins/typed'
   ],
 
